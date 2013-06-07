0c8638d7b55072c55eaea6dfc2ec828e4ab72c08
Andrew Crites <explosion-pills@aysites.com>
2013-06-06 21:57:50 -0400

    Store game_id as part of Game object

--- server/Game.js
+++ server/Game.js
@@ -3,24 +3,24 @@ Game = function () {
 Game.prototype = {
     init: function () {
         // create a new game w/ fresh board
-        var game_id = Games.insert({board: new_board()});
-        var game_clock_id = GameClocks.insert({game_id: game_id, clock: 3});
+        this.game_id = Games.insert({board: new_board()});
+        this.game_clock_id = GameClocks.insert({game_id: this.game_id, clock: 3});
 
         // move everyone who is ready in the lobby to the game
         Players.update({game_id: null, idle: false, name: {$ne: ''}},
-                       {$set: {game_id: game_id}},
+                       {$set: {game_id: this.game_id}},
                        {multi: true});
         // Save a record of who is in the game, so when they leave we can
         // still show them.
-        var p = Players.find({game_id: game_id},
+        var p = Players.find({game_id: this.game_id},
                              {fields: {_id: true, name: true}}).fetch();
-        Games.update({_id: game_id}, {$set: {players: p}});
+        Games.update({_id: this.game_id}, {$set: {players: p}});
 
         // wind down the game clock
         var clock = 3;
         var interval = Meteor.setInterval(function () {
           clock -= 1;
-          GameClocks.update({game_id: game_id}, {$set: {clock: clock}});
+          GameClocks.update({game_id: this.game_id}, {$set: {clock: clock}});
 
           // end of game
           if (clock === 0) {
@@ -28,7 +28,7 @@ Game.prototype = {
             Meteor.clearInterval(interval);
             // declare zero or more winners
             var scores = {};
-            Words.find({game_id: game_id}).forEach(function (word) {
+            Words.find({game_id: this.game_id}).forEach(function (word) {
               if (!scores[word.player_id])
                 scores[word.player_id] = 0;
               scores[word.player_id] += word.score;
@@ -39,10 +39,10 @@ Game.prototype = {
               if (score === high_score)
                 winners.push(player_id);
             });
-            Games.update(game_id, {$set: {winners: winners}});
+            Games.update(this.game_id, {$set: {winners: winners}});
           }
         }, 1000);
 
-        return game_id;
+        return this.game_id;
     }
 }
